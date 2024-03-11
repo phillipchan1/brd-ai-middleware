a4ba6a503b2d3708bbcb66bbd857c1bc71b8b3c7
Phillip Chan <phillipchan@Phillips-MacBook-Pro.local>
2024-03-11 11:16:44 -0700

    Update the port to default to process.env

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import BRDGenerator from './brd-generator/brd-generator';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
